3b1dff3cbeb535ecb46f0ee4911c1022d6457741
Keiven <84637577+keiven0219@users.noreply.github.com>
2023-02-28 15:40:20 +0800

    Update 0_000快捷方式.py

--- 0_000快捷方式.py
+++ 0_000快捷方式.py
@@ -3,21 +3,3 @@
 # table          table
 # shift+table    撤销table
 
-# import sys
-# print(sys.getdefaultencoding())
-# for i in range(0,10):
-#     print(i,end='-')
-
-# num_list = [1, 2, 3, 4, 5, 6]
-# it = iter(num_list) #迭代器 next（）
-# for num in it:
-#     print(num,end='000')
-
-import sys
-num_list = [1, 2, 3, 4, 5, 6]
-it = iter(num_list) #迭代器 next（）
-while True:
-    try:
-        print(next(it))
-    except StopIteration:
-        sys.exit()
